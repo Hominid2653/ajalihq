f736209435b54a9254859ed45720eec3fceb6b23
Nina Adora <nirinakaes@gmail.com>
2026-08-20 13:04:26 +0300

    Wrap App component with BrowserRouter for routing support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import { ThemeProvider } from "next-themes"
 import { Toaster } from "@/components/ui/sonner"
 import { TooltipProvider } from "@/components/ui/tooltip"
@@ -10,7 +11,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <TooltipProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
         <Toaster />
       </TooltipProvider>
     </ThemeProvider>
